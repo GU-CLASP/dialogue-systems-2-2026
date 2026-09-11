41e04af078526e2fad63f71f9a617620427697d8
cschuene-dev <gusschunju@student.gu.se>
2026-09-11 14:01:11 +0200

    added dm.ts

---
+++ labs/lab1/src/dm.ts.ts
@@ -0,0 +1,149 @@
+import { assign, createActor, fromPromise, setup } from "xstate";
+import { Settings, speechstate } from "speechstate";
+import { KEY } from "./credentials";
+import { DMContext, DMEvents } from "./types";
+import OpenAI from "openai";
+
+const REGION = "<YOUR_REGION>";
+
+const openai = new OpenAI({
+  baseURL: "http://localhost:11434/v1/",
+  apiKey: "ollama",
+  dangerouslyAllowBrowser: true,
+});
+
+
+
+const settings: Settings = {
+  asrDefaultCompleteTimeout: 0,
+  asrDefaultNoInputTimeout: 5000,
+  bargeIn: false,
+};
+
+interface GrammarEntry {
+
+}
+
+const grammar: { [index: string]: GrammarEntry } = {
+
+};
+
+function isInGrammar(utterance: string) {
+  return utterance.toLowerCase() in grammar;
+}
+
+const dmMachine = setup({
+  types: {
+    /** you might need to extend these */
+    context: {} as DMContext,
+    events: {} as DMEvents,
+  },
+  actions: {
+    /** define your actions here */
+    "spst.speak": ({ context }, params: { utterance: string }) =>
+      context.spstRef.send({
+        type: "SPEAK",
+        value: {
+          utterance: params.utterance,
+        },
+      }),
+    "spst.listen": ({ context }) =>
+      context.spstRef.send({
+        type: "LISTEN",
+      }),
+  },
+  actors: {},
+}).createMachine({
+  context: ({ spawn }) => ({
+    spstRef: spawn(speechstate, { input: settings }),
+    lastResult: null,
+  }),
+  id: "DM",
+  initial: "Prepare",
+  states: {
+    Prepare: {
+      entry: ({ context }) => context.spstRef.send({ type: "PREPARE" }),
+      on: { ASRTTS_READY: "WaitToStart" },
+    },
+    WaitToStart: {
+      on: { CLICK: "Greeting" },
+    },
+    Greeting: {
+      initial: "Prompt",
+      on: {
+        LISTEN_COMPLETE: [
+          {
+            target: "CheckGrammar",
+            guard: ({ context }) => !!context.lastResult,
+          },
+          { target: ".NoInput" },
+        ],
+      },
+      states: {
+        Prompt: {
+          entry: { type: "spst.speak", params: { utterance: `Hello world!` } },
+          on: { SPEAK_COMPLETE: "Ask" },
+        },
+        NoInput: {
+          entry: {
+            type: "spst.speak",
+            params: { utterance: `I can't hear you!` },
+          },
+          on: { SPEAK_COMPLETE: "Ask" },
+        },
+        Ask: {
+          entry: { type: "spst.listen" },
+          on: {
+            RECOGNISED: {
+              actions: assign(({ event }) => {
+                return { lastResult: event.value };
+              }),
+            },
+            ASR_NOINPUT: {
+              actions: assign({ lastResult: null }),
+            },
+          },
+        },
+      },
+    },
+    CheckGrammar: {
+      entry: {
+        type: "spst.speak",
+        params: ({ context }) => ({
+          utterance: `You just said: ${context.lastResult![0].utterance}. And it ${
+            isInGrammar(context.lastResult![0].utterance) ? "is" : "is not"
+          } in the grammar.`,
+        }),
+      },
+      on: { SPEAK_COMPLETE: "Done" },
+    },
+    Done: {
+      on: {
+        CLICK: "Greeting",
+      },
+    },
+  },
+});
+
+const dmActor = createActor(dmMachine, {}).start();
+
+dmActor.subscribe((state) => {
+  console.group("State update");
+  console.log("State value:", state.value);
+  console.log("State context:", state.context);
+  console.groupEnd();
+});
+
+export function setupButton(element: HTMLButtonElement) {
+  element.addEventListener("click", () => {
+    dmActor.send({ type: "CLICK" });
+  });
+  dmActor.subscribe((snapshot) => {
+    const meta: { view?: string } = Object.values(
+      snapshot.context.spstRef.getSnapshot().getMeta(),
+    )[0] || {
+      view: undefined,
+    };
+    element.innerHTML = `${meta.view}`;
+  });
+}
